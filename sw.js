1885b84c346db26ed9d72c9b6383d8f1dfd47ea0
shprot133 <eriklishchuk5@gmail.com>
2026-06-23 00:00:29 +0300

    Export edited tracker data for GitHub

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const CACHE_PREFIX = "elden-ring-tracker-";
-const DATA_URL = "./tracker-data.js?v=20260622-03";
+const DATA_URL = "./tracker-data.js?v=20260622-04";
 const ASSETS = [
   "./",
   "./index.html",
