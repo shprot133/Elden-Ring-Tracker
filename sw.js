c5fbc66767ad58a29ebcae1c7f61bb27278a7060
shprot133 <eriklishchuk5@gmail.com>
2026-06-23 05:42:03 +0300

    Animate progress and add interaction sounds

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const CACHE_PREFIX = "elden-ring-tracker-";
-const DATA_URL = "./tracker-data.js?v=20260623-02";
+const DATA_URL = "./tracker-data.js?v=20260623-03";
 const ASSETS = [
   "./",
   "./index.html",
